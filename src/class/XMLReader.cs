2079552eee023d1fac3f314f351d0f6e38dbbbc8
lily <char73@pm.me>
2024-03-26 17:09:52 +0000

    fix: :bug: xml load now fails nicer

--- src/class/XMLReader.cs
+++ src/class/XMLReader.cs
@@ -224,7 +224,17 @@ namespace RimDef
       }
 
       var doc = new XmlDocument();
-      doc.Load(path);
+
+      try
+      {
+        doc.Load(path);
+      }
+      catch (Exception e)
+      {
+        Debug.Fail("invalid mod config for " + path + "  -  " + e);
+        return activeMods;
+      }
+
       foreach (
           XmlNode node in doc.DocumentElement.SelectNodes("/ModsConfigData/activeMods/li")
       )
@@ -241,7 +251,17 @@ namespace RimDef
         Debug.Fail("file " + file + " does not exist?");
         return packageId;
       }
-      doc.Load(file);
+
+      try
+      {
+        doc.Load(file);
+      }
+      catch (Exception e)
+      {
+        Debug.Fail("invalid package id for " + file + "  -  " + e);
+        return "error";
+      }
+
       XmlNode node = doc.DocumentElement.SelectSingleNode("/ModMetaData/packageId");
       if (node != null)
         packageId = node.InnerText.ToLower();
@@ -257,7 +277,17 @@ namespace RimDef
         Debug.Fail("file " + file + " does not exist?");
         return modName;
       }
-      doc.Load(file);
+
+      try
+      {
+        doc.Load(file);
+      }
+      catch (Exception e)
+      {
+        Debug.Fail("invalid mod name for " + file + "  -  " + e);
+        return "error";
+      }
+
       XmlNode node = doc.DocumentElement.SelectSingleNode("/ModMetaData/name");
       if (node != null)
         modName = node.InnerText.ToLower();
@@ -270,7 +300,17 @@ namespace RimDef
 
       var doc = new XmlDocument();
       //Console.WriteLine(file);
-      doc.Load(file);
+
+      try
+      {
+        doc.Load(file);
+      }
+      catch (Exception e)
+      {
+        Debug.Fail("invalid xml for " + file + "  -  " + e);
+        return xmlDefs;
+      }
+
       foreach (XmlNode node in doc.DocumentElement.SelectNodes("/Defs"))
       {
         foreach (XmlNode child in node.ChildNodes)
